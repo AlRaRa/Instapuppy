91ca8d146b68d1e01f51b383076e1ff180ab6af4
wirecobweb <wirecobweb@gmail.com>
2019-09-07 22:21:29 +0200

    interface component

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,10 +1,26 @@
 import * as React from 'react'
+interface IIntroProps{
+  text: string
 
-export default class Intro extends React.Component{
+}
+
+
+
+export default class Intro extends React.Component<IIntroProps>{
+
+
+  public state = {
+    text:'Soy el texto del estado'
+  }
     public render(){
+
+
+        const {text} = this.props
+        const t = text ? text : this.state.text 
+
         return(
         <p className="App-intro">
-        <span>Hola</span>
+        <span>{t}</span>
       </p>
         )
     }

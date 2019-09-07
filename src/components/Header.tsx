0904ea68cccd98924f1161084acc5823577b5257
wirecobweb <wirecobweb@gmail.com>
2019-09-07 18:12:41 +0200

    add

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import logo from '../logo.svg'
 
 
-export class Header extends React.Component{
+export default class Header extends React.Component{
     public render(){
         return(
             <header className="App-header">

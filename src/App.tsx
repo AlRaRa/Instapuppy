55f1e6de33518e580fc6aaadd9d8036a14d61daa
wirecobweb <wirecobweb@gmail.com>
2019-09-11 10:29:54 +0200

    Center

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,20 @@ import './App.css';
 import Card from './components/Card';
 import Container from './components/Container'
 import Input from './components/Input';
-import Button from './components/Button'
-
+import Button from './components/Button';
+import Title from './components/Tittle';
+import Link from './components/Link'
 
 class App extends React.Component {
   public render() {
     return (
      <Container>
         <Card>
-
+          <Title>Iniciar Sesion</Title>
           <Input placeholder='Correo' Label='Correo'/>
           <Input placeholder='Contraseña' Label='Contraseña'/>
-          <Button>Enviar</Button>
+          <Button block={true}>Enviar</Button>
+          <Link>Ir al Registro</Link>
         </Card>
      </Container>
        
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,27 @@
 import * as React from 'react'
 
 
-
-
-const style = {
+const style= (block: boolean) => ({
     backgroundColor:'#00D182',
     border: '0PX',
     borderRadius: '4px',
     padding:'10px 15px',
+    color:'#fff',
+    width: block ? '100%' : undefined
+})
+
+interface IButtonProps{
+    block?: boolean
 }
 
-export default class Card extends React.Component{
+
+
+export default class Button extends React.Component<IButtonProps>{
 
     public render(){
-
+        const {block = false} = this.props
         return(
-           <button {...this.props} style={style}/>
+           <button {...this.props} style={style(block)}/>
         )
     }
 }

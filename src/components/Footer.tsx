497056eb5375bc02ead9f2fb585cea7de4acf209
wirecobweb <wirecobweb@gmail.com>
2019-09-11 20:10:32 +0200

    footer fail

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,27 +3,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faThumbsUp, faRetweet } from '@fortawesome/free-solid-svg-icons';
 
 
-
-const style = {
-    backgroundColor: '#fff',
-    border: '1px solid #ddd',
-    padding: '10px 15px'
-}
-
-interface IPostProps{
-    image: string
-}
-export default class Post extends React.Component<IPostProps>{
+export default class Footer{
     public render(){
-        const { image } = this.props
         return(
-           <div style={style}>
-               <img src={image} />
-               <div style={{ display:'flex', backgroundColor:'#eee', marginLeft: '-15px', marginBottom: '-10px', width:'calc(100% +30px)'}}>
+            
+            <div style={{ display:'flex', backgroundColor:'#eee', marginLeft: '-15px', marginBottom: '-10px', width:'calc(100% +30px)'}}>
                    <div style={{flex:1, textAlign:'center', padding:'10px 15px' , cursor: 'pointer'}}><FontAwesomeIcon icon={faThumbsUp} /> Like</div>
                    <div style={{flex:1, textAlign:'center', padding:'10px 15px', cursor:'pointer'}}><FontAwesomeIcon icon={faRetweet} /> Compartir</div>
-               </div>
-            </div>   
+            </div>
         )
     }
 }

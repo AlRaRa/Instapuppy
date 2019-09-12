4392d7e72cc81330bca1efb5c18e5e0a79a1a7fa
wirecobweb <wirecobweb@gmail.com>
2019-09-12 15:53:01 +0200

    cutre navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,27 @@
 import * as React from 'react'
 import {Link} from 'react-router-dom'
+import {faUser, faNewspaper} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+
+
+const style = {
+
+    navbar:{
+    borderBotton: 'solid 1px #aaa',
+    padding: '10px 15px'
+    },
+    Link: {
+       color:'#555',
+       padding: '10px 15px',
+    }
+}
+
 export default class Navbar extends React.Component{
     public render(){
         return(
-            <div style={style}>
-                <Link to='/app/newsfeed'>Instapuppy</Link>
-                <Link to='/app/profile'>PerfiL</Link>'
+            <div style={style.navbar}>
+                <div><Link style={style.Link} to='/app/newsfeed'><FontAwesomeIcon icon={faNewspaper}/>Instapuppy</Link></div>
+                <div><Link to='/app/profile' style={style.navbar}><FontAwesomeIcon  icon={faUser}/>PerfiL</Link></div>
             </div>
         )
     }

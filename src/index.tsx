5bb7d413474bb8c933a8cfb4a361e8d2c4e06d4e
Rafa <wirecobweb@gmail.com>
2019-09-12 18:59:44 +0200

    thunk ok

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,9 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Router} from 'react-router'
 import {Provider} from 'react-redux'
-import {createStore, combineReducers} from 'redux'
+import {createStore, combineReducers, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+
 import * as reducers from './ducks'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -15,7 +17,7 @@ import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(combineReducers({
   ...reducers,
-}))
+}),applyMiddleware(thunk))
 
 const history = createHistory()
 

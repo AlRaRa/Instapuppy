0946a346d405da457c32bbe5ffed9dc4b3493d65
Rafa <wirecobweb@gmail.com>
2019-09-12 18:58:02 +0200

    ducks

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,15 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Router} from 'react-router'
 import {Provider} from 'react-redux'
-import {createStore} from 'redux'
+import {createStore, combineReducers} from 'redux'
+import * as reducers from './ducks'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 
-const store = createStore()
+const store = createStore(combineReducers({
+  ...reducers,
+}))
 
 const history = createHistory()
 

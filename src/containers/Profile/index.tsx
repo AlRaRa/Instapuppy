788ad48b82e47346ba1a403c354bad72ccf9e258
Rafa <wirecobweb@gmail.com>
2019-09-12 18:04:31 +0200

    card

--- src/containers/Profile/index.tsx
+++ src/containers/Profile/index.tsx
@@ -1,20 +1,37 @@
 import * as React from 'react'
 import ProfileImg from 'src/components/ProfileImg';
-
+import Button from 'src/components/Button';
+import Card from '../../components/Card'
 
 
 const style ={
+
     container:{
-        padding: '15px',
+        padding: '15px'
+    },
+    row:{
+        dipslay: 'flex',
+        justifyContent:'space-between',
+        padding:'15px',
     }
 }
 export default class Profile extends React.Component{
     public render(){
         return(
-            <div style={style.container}>
+            <div style={style.container}> 
+                <div style={style.row}>
 
                 <ProfileImg/>
+                <Button>Agregar</Button>
+                </div>
+            
+            <div style={style.row}>
+                <Card>Img 1</Card>
+                <Card>Img 2</Card>
+                <Card>Img 3</Card>
             </div>
+            </div>
+           
         )
     }
 }

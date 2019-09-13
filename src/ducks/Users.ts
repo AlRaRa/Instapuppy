c26d74d2a24e2546cebad99c56ed5dd25588ac93
Rafa <wirecobweb@gmail.com>
2019-09-13 18:53:06 +0200

    services in

--- src/ducks/Users.ts
+++ src/ducks/Users.ts
@@ -1,3 +1,16 @@
+import { Dispatch } from 'redux';
+import {IServices} from '../services'
+interface ILogin{
+    email: string
+    password: string
+}
+
 export default function reducer(state = {}){
     return state 
+}
+
+
+export default login = (payload: ILogin) =>
+(dispatch: Dispatch, getState: ()=> any, { auth}: IServices)=>{
+    
 }

3cf864d0500e36cb1a7aa28da843a1a854c577c7
Rafa <wirecobweb@gmail.com>
2019-09-14 10:54:51 +0200

    login field

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -4,7 +4,7 @@ import Center from './Center';
 import { Link } from 'react-router-dom';
 import Button from './Button';
 
-import {reduxForm, InjectedFormProps} from 'redux-form'
+import {reduxForm, InjectedFormProps, Field} from 'redux-form'
 
 class LoginForm extends React.Component<InjectedFormProps>{
     public render(){
@@ -12,8 +12,8 @@ class LoginForm extends React.Component<InjectedFormProps>{
         const {handleSubmit} = this.props
         return(
             <form onSubmit={handleSubmit}>
-              <Input placeholder='Correo' label='Correo'/>
-              <Input placeholder='Contraseña' label='Contraseña'/>
+              <Field label='Correo' placeholder='Correo' name='email' type='email' component={Input}/>
+              <Field label='Correo' placeholder='Correo' name='password' type='password' component={Input}/>
               <Button block={true}>Enviar</Button>
               <Center>
               <Link to='/register'>Ir al registro</Link>
--- src/containers/Auth/Login.tsx
+++ src/containers/Auth/Login.tsx
@@ -4,19 +4,21 @@ import Container from '../../components/Container'
 import Title from '../../components/Tittle';
 import LoginForm from '../../components/LoginForm'
 import {connect} from 'react-redux';
-import {login as loginThunk} from '../../ducks/Users'
+import {login as loginThunk, ILogin} from '../../ducks/Users'
 
 
 interface ILoginProps {
-  login: (a: ILogin)
+  login: (a: ILogin) => void
 }
-  class Login extends React.Component{
+  class Login extends React.Component<ILoginProps>{
+
     public render(){
+      const {login} = this.props
         return(
             <Container center={true}>
             <Card>
               <Title>Iniciar Sesion</Title>
-              <LoginForm />
+              <LoginForm onSubmit={login}/>
             </Card>
          </Container>
         )
